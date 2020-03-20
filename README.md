cd7e78940d22747ab3cd4cf30cd7f8807bd5d841
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2020-03-20 14:24:42 +0000

    Updated with rate of confirmed cases

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # covid-19
 Dashboard visualising Public Health England coronavirus data
+
+---
+
+#### Data sources
+
+- [latest confirmed cases by country and total UK deaths](https://www.arcgis.com/sharing/rest/content/items/bc8ee90225644ef7a6f4dd1b13ea1d67/data)
+- [time series of daily confirmed cases](https://www.arcgis.com/sharing/rest/content/items/e5fd11150d274bebaaf8fe2a7a2bda11/data)
+- [confirmed cases by Upper Tier Local Authority in England](https://www.arcgis.com/sharing/rest/content/items/b684319181f94875a6879bbc833ca3a6/data)
--- data/pre-processing.R
+++ data/pre-processing.R
@@ -1,8 +1,8 @@
-# Vector boundaries for countries and UTLAs in England #
+# Vector boundaries for countries and UTLAs in England with mid-2018 population estimates #
 
 library(tidyverse) ; library(sf)
 
-# Northern Ireland, Scotland, Wales
+# Northern Ireland, Scotland, and Wales
 # Source: Open Geography Portal, ONS
 # URL: https://geoportal.statistics.gov.uk/datasets/countries-december-2018-boundaries-uk-bgc
 
@@ -17,5 +17,13 @@ countries <- st_read("https://ons-inspire.esriuk.com/arcgis/rest/services/Admini
 utla <- st_read("https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Counties_and_Unitary_Authorities_April_2019_Boundaries_EW_BUC/MapServer/0/query?where=UPPER(ctyua19cd)%20like%20'%25E_%25'&outFields=*&outSR=4326&f=geojson") %>% 
   select(area_code = ctyua19cd, area_name = ctyua19nm, long, lat)
 
+# Mid-2018 population estimates
+# Source: Nomis / ONS
+# URL: https://www.nomisweb.co.uk/query/construct/summary.asp?mode=construct&version=0&dataset=2002
+population <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=2092957702,2092957701,2092957700,1816133633...1816133848&date=latest&gender=0&c_age=200&measures=20100&select=geography_code,obs_value") %>% 
+  rename(area_code = GEOGRAPHY_CODE, population = OBS_VALUE)
+
+# combine UTLA and country polygons and join population estimates
 rbind(countries, utla) %>%
+  left_join(population, by = "area_code") %>% 
   st_write("areas.geojson")
