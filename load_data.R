70a5089488f3077746ecd1c0b5bd6c75db19d2bc
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2020-04-22 17:28:42 +0100

    Confirmed cases in Trafford

--- load_data.R
+++ load_data.R
@@ -1,6 +1,48 @@
 library(tidyverse) ; library(jsonlite)
 
 # -------------------------------------------
+# Mid-2018 population estimates
+# -------------------------------------------
+
+# Source: Nomis / ONS
+# URL: https://www.nomisweb.co.uk/query/construct/summary.asp?mode=construct&version=0&dataset=2002
+
+# read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1816133652...1816133661&date=latest&gender=0&c_age=200&measures=20100&select=date_name,geography_name,geography_code,gender_name,c_age_name,measures_name,obs_value,obs_status_name") %>%
+#   select(AreaCode = GEOGRAPHY_CODE, Persons = OBS_VALUE) %>%
+#   write_csv("data/mid-2018_population_estimates.csv")
+
+population <- read_csv("data/mid-2018_population_estimates.csv",
+                       col_types = cols(
+                         AreaCode = col_character(),
+                         Persons = col_integer()))
+
+# -------------------------------------------
+# Greater Manchester cases
+# -------------------------------------------
+
+# Source: Public Health England
+# URL: https://coronavirus.data.gov.uk
+
+gm_data <- read_csv("https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv",
+                          col_types = cols(
+                            `Area name` = col_character(),
+                            `Area code` = col_character(),
+                            `Area type` = col_character(),
+                            `Specimen date` = col_date(format = "%Y-%m-%d"),
+                            `Daily lab-confirmed cases` = col_integer(),
+                            `Cumulative lab-confirmed cases` = col_integer())) %>% 
+  select(Date = `Specimen date`,
+         AreaName = `Area name`,
+         AreaCode = `Area code`,
+         AreaType = `Area type`,
+         NewCases = `Daily lab-confirmed cases`,
+         CumCases = `Cumulative lab-confirmed cases`) %>% 
+  filter(AreaName %in% c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", "Salford", "Stockport", "Tameside", "Trafford", "Wigan")) %>%
+  arrange(Date) %>% 
+  left_join(population, by = "AreaCode") %>% 
+  mutate(Rate = round((CumCases/Persons*10000),1))
+
+# -------------------------------------------
 # UK cases and deaths
 # -------------------------------------------
 
@@ -66,6 +108,7 @@ country_data <- filter(country_data, days <= shortened_days)
 
 # Source: Blavatnik School of Government, Oxford University
 # URL: https://www.bsg.ox.ac.uk/research/research-projects/oxford-covid-19-government-response-tracker
+
 stringency_index <- fromJSON(paste0("https://covidtrackerapi.bsg.ox.ac.uk/api/stringency/date-range/2020-01-31/", Sys.Date()), flatten = TRUE) %>% 
   pluck("data") %>% 
   enframe() %>%
