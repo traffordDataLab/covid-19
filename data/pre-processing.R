0e576c6ac43bef876fd66ce2da4da74afc501ddb
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2020-06-17 14:16:33 +0100

    Mid-2019 population estimates

--- data/pre-processing.R
+++ data/pre-processing.R
@@ -1,4 +1,4 @@
-library(tidyverse) ; library(sf)
+library(tidyverse) ; library(sf) ; library(readxl) ; library(httr)
 
 # -------------------------------------------
 # Geospatial data
@@ -11,7 +11,7 @@ library(tidyverse) ; library(sf)
 # NB dissolve polygons for Cornwall and Isles of Scilly
 
 lad <- st_read("https://opendata.arcgis.com/datasets/3a4fa2ce68f642e399b4de07643eeed3_0.geojson") %>% 
-  select(area_code = lad19cd, area_name = lad19nm, long, lat, st_area.shape.) %>% 
+  select(area_code = lad19cd, area_name = lad19nm, long, lat, st_areashape) %>% 
   filter(str_detect(area_code, "^E")) %>% 
   mutate(area_code = as.character(area_code),
          area_code = case_when(area_code %in% c("E06000052", "E06000053") ~ "E06000052", TRUE ~ area_code),
@@ -20,21 +20,27 @@ lad <- st_read("https://opendata.arcgis.com/datasets/3a4fa2ce68f642e399b4de07643
          long = case_when(area_code == "E06000052" ~ -4.64249, TRUE ~ long),
          lat = case_when(area_code == "E06000052" ~ 50.45023, TRUE ~ lat)) %>% 
   group_by(area_name, area_code, long, lat) %>% 
-  summarise(st_area.shape. = sum(st_area.shape.)) %>% 
-  select(-st_area.shape.)
+  summarise(st_areashape = sum(st_areashape)) %>% 
+  select(-st_areashape)
 
 # -------------------------------------------
 # Population estimates
 # ------------------------------------------- 
 
-# Mid-2018 population estimates
-# Source: Nomis / ONS
-# URL: https://www.nomisweb.co.uk/datasets/pestsyoala
+# Mid-2019 population estimates
+# Source: ONS
+# URL: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/populationestimatesforukenglandandwalesscotlandandnorthernireland
 
 # NB combine population estimates for Cornwall and Isles of Scilly
 
-population <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=1820327937...1820328318&date=latest&gender=0&c_age=200&measures=20100&select=geography_code,obs_value") %>% 
-  rename(area_code = GEOGRAPHY_CODE, population = OBS_VALUE) %>% 
+tmp <- tempfile(fileext = ".xls")
+GET(url = "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid2019april2020localauthoritydistrictcodes/ukmidyearestimates20192020ladcodes.xls",
+    write_disk(tmp))
+
+population <- read_xls(tmp, sheet = 6, range = "A5:D420") %>% 
+  filter(Geography1 %in% c("London Borough", "Metropolitan District", 
+                           "Non-metropolitan District", "Unitary Authority")) %>% 
+  select(area_code = Code, population = `All ages`) %>% 
   mutate(area_code = case_when(as.character(area_code) %in% c("E06000052", "E06000053") ~ "E06000052", TRUE ~ area_code)) %>% 
   group_by(area_code) %>% 
   summarise(population = sum(population))
