8cccbce47f1de0b97b8b7f1c820c391556983506
Iris Garcia Rios <iris.garciarios@trafford.gov.uk>
2021-04-29 08:31:26 +0100

    Amendments to handle zero weekly cases

--- global.R
+++ global.R
@@ -18,14 +18,22 @@ cases <- read_csv("https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cas
   ungroup() %>% 
   fill(area_name)
 
-# MSOAs in England
-# Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-ew-bgc
-msoa <- st_read("data/msoa.geojson")
 
 # MSOA lookup
 # Source: ONS Open Geography Portal; House of Commons Library
 msoa_lookup <- read_csv("data/msoa_lookup.csv")
+
+# MSOAs in England
+# Source: ONS Open Geography Portal
+# URL: https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-ew-bgc
+msoa <- st_read("data/msoa.geojson") %>%
+  left_join(msoa_lookup, by = "msoa11cd") %>% 
+  # combine Hackney and City of London / Cornwall and Isles of Scilly 
+  mutate(lad19nm = as.character(lad19nm),
+         lad19nm = case_when(
+           lad19nm %in% c("Cornwall", "Isles of Scilly") ~ "Cornwall and Isles of Scilly",
+           lad19nm %in% c("City of London", "Hackney") ~ "Hackney and City of London", 
+           TRUE ~ lad19nm))
 
 # Latest 7 days of cases by MSOA
 # Source: Public Health England
@@ -34,15 +42,8 @@ msoa_cases <- read_csv("https://api.coronavirus.data.gov.uk/v2/data?areaType=mso
   filter(date == max(date)) %>% 
   select(msoa11cd = areaCode, date, n = newCasesBySpecimenDateRollingSum, rate = newCasesBySpecimenDateRollingRate) %>% 
   mutate(n = replace_na(n, 0),
-         rate = replace_na(rate, 0)) %>% 
-  left_join(msoa_lookup, by = "msoa11cd") %>% 
-  select(date, msoa11cd, msoa11hclnm, lad19cd, lad19nm, n, rate) %>% 
-  # combine Hackney and City of London / Cornwall and Isles of Scilly 
-  mutate(lad19nm = as.character(lad19nm),
-         lad19nm = case_when(
-    lad19nm %in% c("Cornwall", "Isles of Scilly") ~ "Cornwall and Isles of Scilly",
-    lad19nm %in% c("City of London", "Hackney") ~ "Hackney and City of London", 
-    TRUE ~ lad19nm))
+         rate = replace_na(rate, 0)) 
+
 
 # Mid-2019 population estimates
 # Source: Nomis / ONS
